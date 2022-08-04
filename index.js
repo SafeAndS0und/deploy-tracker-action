8e6edbee08d658003b12ecda20bd5110dc572f32
Nikodem Lorenz <nikodem.lorenz@gmail.com>
2022-08-04 12:58:47 +0200

    trying out javascript github action

---
+++ index.js
@@ -0,0 +1,14 @@
+import core from '@actions/core'
+import github from '@actions/github'
+import fetch from 'node-fetch'
+
+try {
+    const application = core.getInput('application');
+    const branch = core.getInput('branch');
+    const triggeredBy = core.getInput('triggeredBy');
+    const message = core.getInput('message');
+    console.log(application, branch, triggeredBy, message);
+    console.log(JSON.stringify(github.context, undefined, 2));
+} catch (error) {
+    core.setFailed(error.message);
+}
