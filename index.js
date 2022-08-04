88c78f63862c5fb24bb535b1841f565b01a78ce5
Nikodem Lorenz <nikodem.lorenz@gmail.com>
2022-08-04 13:47:02 +0200

    attempt to trigger actual notification

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ import core from '@actions/core'
 import github from '@actions/github'
 // import fetch from 'node-fetch'
 
-try {
-    const dtAccessToken = core.getInput('dt-access-token');
-    const application = core.getInput('application');
-    const branch = core.getInput('branch');
-    const triggeredBy = core.getInput('triggeredBy');
-    const message = core.getInput('message');
-    console.log(dtAccessToken, application, branch, triggeredBy, message);
-} catch (error) {
-    core.setFailed(error.message);
+const main = async () => {
+    try {
+        const dtAccessToken = core.getInput('dt-access-token');
+        const application = core.getInput('application');
+        const branch = core.getInput('branch');
+        const triggeredBy = core.getInput('triggeredBy');
+        const message = core.getInput('message');
+
+        const response = await fetch("https://api.deploytracker.io/notify", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "dt-access-token": dtAccessToken,
+            },
+            body: JSON.stringify({
+                application,
+                triggeredBy,
+                branch,
+                message,
+                version: "1.0.0",
+                status: "SUCCESS",
+            }),
+        });
+        console.log(response);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+main()
